77c5d2fbd212652759d9e18949048234441c6543
KovalevskaOlga <olgggavolnaja@gmail.com>
2024-11-22 22:35:51 +0200

    hw8-js

---
+++ hw.js
@@ -0,0 +1,17 @@
+//task 1
+// const fruits = ['apple', 'banana', 'cherry'];
+// const fruit = fruits.join(", ");
+// console.log(fruit)
+
+//task 2
+const cards = ['Карточка-1', 'Карточка-2', 'Карточка-3', 'Карточка-4', 'Карточка-5'];
+// const deleteCard3 = cards.splice(2, 1);
+// console.log(cards);
+
+//task 3
+// const addCards6 = cards.push('Карточка-6');
+// console.log(cards);
+
+//task  4
+const updateCard4 = cards.splice(3, 1, 'CardsNew-4');
+console.log(cards);
